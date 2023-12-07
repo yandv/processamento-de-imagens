7bd947d23cef8cfa94f2f4a3748d73dfee8ff754
yandv <allanmxr@gmail.com>
2023-12-06 22:07:50 -0300

    Last commit

--- src/cleber-ofc.py
+++ src/cleber-ofc.py
@@ -1,6 +1,7 @@
 import cv2
 import pytesseract as tesseract
 import PIL as Image
+import re
 
 def pre_processamento(imagem) -> cv2.typing.MatLike:
     preProcessedImage = cv2.resize(imagem, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
@@ -12,7 +13,16 @@ def pre_processamento(imagem) -> cv2.typing.MatLike:
 
     # Aplica um threshold para binarizar a imagem
     #preProcessedImage = cv2.threshold(preProcessedImage, 0, 255, cv2.THRESH_BINARY)[1]
-    preProcessedImage = cv2.adaptiveThreshold(preProcessedImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 41, 10);
+    preProcessedImage = cv2.adaptiveThreshold(preProcessedImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 41, 10)
+
+    # Aumenta o contraste da imagem para melhorar a segmentação dos caracteres da imagem binarizada (opcional)
+    preProcessedImage = cv2.addWeighted(preProcessedImage, 1.5, preProcessedImage, -0.5, 0)
+
+    # Dilatação da imagem 
+    preProcessedImage = cv2.dilate(preProcessedImage, None, iterations=1)
+
+    # Errosão da imagem
+    preProcessedImage = cv2.erode(preProcessedImage, None, iterations=1)
 
     return preProcessedImage
 
@@ -44,39 +54,50 @@ def reconhecer_caractere(caractere):
 
 def pos_processamento(textos):
     # Remove espaços em branco extras dos textos
-    #textos = [text.strip() for text in textos]
+    textos = [text.strip() for text in textos]
 
     # Retorna os textos
+    
+    textos = [text for text in textos if text]
+
+    # Remove os textos vazios
+
+    textos = [text for text in textos if len(text) > 1]
+
+    # regex
+
+    textos = [re.sub(r'[^\w\s]|_+$', '', text) for text in textos]
+
+    # juntar palavras
+
+    textos = [''.join(text.split()) for text in textos]
+
+    # splitar palavras
+
+    textos = [re.sub(r"([A-Z])", r" \1", text).split() for text in textos]
+
     return textos
 
 def main():
-    tesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\Tesseract.exe"
-
+    tesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/Tesseract.exe"
     # Carregue a imagem
     photoPath = r"C:\\Users\\User\\.visual-studio-workspace\\processamento-de-imagens\\src\\imagens\\"
     imageName = r"tweet-enner-valencia.jpg"
     realNameImage = imageName.split('.')[0]
-
     # Carrega a imagem
     imagem = cv2.imread(photoPath + imageName)
-
     # Pré-processamento da imagem
     preProcessedImage = pre_processamento(imagem)
 
     cv2.imwrite(photoPath + 'export/' + realNameImage + '-pre-processada.jpg', preProcessedImage)
-
     # Localização do texto
     contornos = localizar_texto(preProcessedImage)
-
     # Segmentação dos caracteres
     caracteres = [segmentar_caracteres(contorno, preProcessedImage) for contorno in contornos]
-
     # Reconhecimento dos caracteres
     textos = [reconhecer_caractere(caractere) for caractere in caracteres]
-
     # Pós-processamento
     textos = pos_processamento(textos)
-
     # Imprime os textos reconhecidos
     for i, texto in enumerate(textos):
         print(f"Texto {i + 1}: {texto}")
